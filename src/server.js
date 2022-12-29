783b9b2fadf3c11341f79872297df26b3b4d7195
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-12-28 22:11:48 -0300

    adding routers in folder server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const app = express()
+
+const database = require("./database/connection")
+const routers = require("./routes/router")
+
+database.connected()
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+app.use("/", routers)
 
 const port = 8080
 
