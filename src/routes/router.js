1adf4297086a525f21081f389a0a852ad08726e6
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-12-28 22:12:43 -0300

    list products ordered

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,7 @@ const ControllerProduct = require("../controller/Product")
 
 
 router.post("/product", ControllerProduct.register)
+router.get("/products", ControllerProduct.list)
 
 module.exports = router
 
