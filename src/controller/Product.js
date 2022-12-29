6ab26dd094bd964299eb149a4d5b8a18c0b3cf16
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-12-28 21:34:50 -0300

    adding function register product

---
+++ src/controller/Product.js
@@ -0,0 +1,16 @@
+const ModelProduct = require("../model/Product")
+
+async function register(req, res){
+  try {
+
+    const {product, desc, valueProduct, available} = req.body
+
+    
+  } catch (error) {
+    
+  }
+}
+
+module.exports ={
+  register,
+}
