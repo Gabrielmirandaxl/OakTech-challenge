39f81288515b92f585d3f39af4c4084e3c9ed639
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2022-12-28 22:14:17 -0300

    adding function list products ordered

--- src/controller/Product.js
+++ src/controller/Product.js
@@ -5,12 +5,34 @@ async function register(req, res){
 
     const {product, desc, valueProduct, available} = req.body
 
+    const productRegister = await ModelProduct.create({
+        product,
+        desc,
+        valueProduct,
+        available
+    })
+
+    return res.status(200).json(productRegister)
+
     
   } catch (error) {
+     return res.status(500).json(error)
+  }
+}
+
+async function list(req, res){
+  try {
     
+  const listProducts = await ModelProduct.find().sort({valueProduct: 1})
+
+  return res.status(200).json(listProducts)
+
+  } catch (error) {
+    return res.status(500).json(error)
   }
 }
 
 module.exports ={
   register,
+  list,
 }
